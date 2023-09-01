9991089133aeebf7a566b99f270efd0aafd65b80
devsamahd <samahd626@gmail.com>
2023-09-01 22:05:26 +0100

    ..

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import styles from './nearbyjobcard.style'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { checkImageURL } from '../../../../utils'
 
-const NearbyJobCard = () => {
+const NearbyJobCard = ({job, handleNavigate}) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
+    <TouchableOpacity style={styles.container} onPress={handleNavigate}>
+      <TouchableOpacity style={styles.logoContainer}>
+        <Image source={{ uri: checkImageURL(job.employer_logo) ? job.employer_logo : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NNICW7diNmexJfMicpY9exHKV4Sg205H.jpg' }} resizeMode='contain' style={styles.logImage} />
+      </TouchableOpacity>
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>{job.job_title}</Text>
+        <Text style={styles.jobType} numberOfLines={1}>{job.job_employment_type}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 

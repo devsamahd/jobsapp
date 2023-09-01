9e767ba0118e601d8847cd3d7e4fdc9826a577d1
devsamahd <samahd626@gmail.com>
2023-09-01 22:05:48 +0100

    ..

--- hooks/useFetch.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react"
-
-const useFetch = () => {
-    const [data, setData] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState(null)
-}

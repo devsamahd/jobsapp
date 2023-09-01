527002e5b408166506c7642099689fdd955cf24c
devsamahd <samahd626@gmail.com>
2023-09-01 22:05:30 +0100

    ...

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './popularjobcard.style'
+import { checkImageURL } from '../../../../utils'
+import { icons } from '../../../../constants'
 
-const PopularJobCard = () => {
+const PopularJobCard = ({item, selectedJob, handlePress}) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
+    <TouchableOpacity style={styles.container(selectedJob, item)} onPress={()=>handlePress(item)}>
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image source={{ uri: checkImageURL(item.employer_logo) ? item.employer_logo : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NNICW7diNmexJfMicpY9exHKV4Sg205H.jpg' }} resizeMode='contain' style={styles.logoImage} />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>{item.job_title}</Text>
+        <Text style={styles.location} numberOfLines={1}>{item.job_country}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
